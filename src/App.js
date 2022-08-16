163f076497cdeeaf7aa7c9cc74e78d0fce4a2d29
Esmatullah Ahmadzai <ea.ahmadzai2020@gmail.com>
2022-08-16 08:54:22 +0430

    Solve linters

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Todos from './components/Todos';
 function App() {
   return (
     <div className="app-container">
-      <div className='container px-4'>
-          <Todos/>
+      <div className="container px-4">
+        <Todos />
       </div>
     </div>
   );
